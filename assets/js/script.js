cec14a3e1e9d6d0d55769adc382b2c77ab02d74b
Hi ASIF (MD ASIF HOSSAIN) <123762960+only1asif4you@users.noreply.github.com>
2024-10-16 11:41:47 +0400

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+/* ======================
+* OWL CAROUSEL / REVIEWS  
+====================== */
+$(".owl-carousel").owlCarousel({
+  loop: true,
+  dots: true,
+  autoplay: true,
+  autoplayTimeout: 2500,
+  autoplaySpeed: 1500,
+  smartSpeed: 500,
+  items: 1,
+});
